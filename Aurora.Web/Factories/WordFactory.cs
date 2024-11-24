388b60032b45e4f5e869adea29f24dffaa691f15
Jess Long <jessica.long@coldbanana.com>
2024-11-24 14:40:47 +0000

    Fix my broken code

--- Aurora.Web/Factories/WordFactory.cs
+++ Aurora.Web/Factories/WordFactory.cs
@@ -18,15 +18,15 @@
                 "Banana", "digital", "quality", "teams", "agency", "home", "office", "awards", "great", "culture", "understand",
                 "support", "personal", "success", "challenges", "impact", "upgrade", "solution", "placement", "modern", "team", "can do",
                 "teamwork", "communication", "creativity"
-            };
-
+            }.Select(x => x.ToLower());
+            
             //Office
             var Office = new List<string> {
                 "Niz", "Ben", "Colgan", "Buiscuit", "Donut", "Mehdi", "Placements", "Hackathon", "Amber", "Carly",
                 "Carl", "Spray Tan", "Pub", "Ping Pong", "Darts", "Freya", "Gen Z Marketing", "Polar Bear", "Owen", "Ollie", "Jess",
                 "Coffee", "Diet Coke", "Tea", "Millie", "Heater", "Pokemon", "Minecraft", "Pint", "Beer", "Fridge", "Water",
                 "Slack", "XMas", "Birthday"
-            };
+            }.Select(x => x.ToLower());
 
             //Dev Hard
             var DevHard = new List<string> {
@@ -34,14 +34,14 @@
                 "dependency", "exception", "interface", "runtime", "Deployment", "Framework", "Full Stack", "Umbraco", "Azure", "Website", "development",
                 "Kentico", "Cyber", "Automation", "testing", "features", "content", "development", "upgrades", "support", "build", "cloud",
                 "Transformation", "products", "services"
-            };
+            }.Select(x => x.ToLower());
 
             //Dev Easy
             var DevEasy = new List<string> {
                 "array", "async", "await", "binary", "bug", "class", "closure", "compile", "database", "debug",
                 "design", "framework", "function", "javascript", "typescript", "json", "lambda", "library", "linq", "method", "null",
                 "object", "syntax", "oop", "variable", "CMS", "API", "Browser", "Code", "Git", "mvc", "Text", "push", "pull", "commit"
-            };
+            }.Select(x => x.ToLower());
 
             allWords.AddRange(Culture);
             allWords.AddRange(DevHard);
