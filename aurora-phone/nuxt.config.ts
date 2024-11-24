4a1f5ad5e535a5daf6151f27abe17042de7b36a5
olliestorey <ollie@coldbananastudio.com>
2024-11-24 15:18:36 +0000

    infra hell

--- aurora-phone/nuxt.config.ts
+++ aurora-phone/nuxt.config.ts
@@ -4,7 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || "https://localhost:7049",
+      apiBase:
+        process.env.NUXT_PUBLIC_API_BASE ||
+        "https://aurora-cjfsgke2ardmfkhg.uksouth-01.azurewebsites.net",
     },
   },
   ssr: false,
